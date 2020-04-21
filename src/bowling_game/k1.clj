ad12c311e3ecb21c9e9a1410f6c713dec0a45a1a
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-21 00:24:55 -0300

    05-test: correctly computing the score of a spare

--- src/bowling_game/k1.clj
+++ src/bowling_game/k1.clj
@@ -8,5 +8,28 @@
 (defn roll [game pins-down]
   (update game :rolls conj pins-down))
 
-(defn score [game]
-  (reduce + (:rolls game)))
+(defn- organize-rolls-into-frame [rolls]
+  (letfn [(rest2 [v] ((comp rest rest) v))]
+    (loop [frame (range 10)
+           roll  rolls
+           res   []]
+      (cond
+        (empty? roll) res
+
+        (= 10 (+ (first roll) (second roll)))
+        (recur (rest frame)
+               (rest2 roll)
+               (conj res {:frame (first frame)
+                          :rolls (take 3 roll)}))
+        :else
+        (recur (rest frame)
+               (rest2 roll)
+               (conj res {:frame (first frame)
+                          :rolls (take 2 roll)}))))))
+
+(defn score [{:keys [rolls]}]
+  (let [frames (organize-rolls-into-frame rolls)]
+    (reduce (fn [acc frame]
+              (+ acc (reduce + (:rolls frame))))
+            0
+            frames)))
--- test/bowling_game/k1_test.clj
+++ test/bowling_game/k1_test.clj
@@ -9,19 +9,22 @@
   (testing "how many frames there is in a single game?"
     (is (= 10 (:total-frames (create-game))))))
 
+(defn game-loop [rolls]
+  (reduce
+   (fn [game pins-down]
+     (roll game pins-down))
+   (create-game)
+   rolls))
+
 (deftest can-we-simulate-game-scores?
   (testing "all the games had throws going to the gutter, therefore 0 scores."
-    (let [game (reduce
-                (fn [game pins-down]
-                  (roll game pins-down))
-                (create-game)
-                (repeat 20 0))]
+    (let [game (game-loop (repeat 20 0))]
       (is (= (score game) 0))))
 
   (testing "let's try to score two rolls."
-    (let [game (reduce
-                (fn [game pins-down]
-                  (roll game pins-down))
-                (create-game)
-                (concat [5 4] (repeat 18 0)))]
-      (is (= (score game) 9)))))
+    (let [game (game-loop (concat [5 4] (repeat 18 0)))]
+      (is (= (score game) 9))))
+
+  (testing "let's face the first special case, the spare."
+    (let [game (game-loop (concat [5 5 3] (repeat 17 0)))]
+      (is (= 16 (score game))))))
