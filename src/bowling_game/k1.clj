32344ecaf5d2eccb4776f7af6b7f17a1e80da8cb
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-20 23:46:20 -0300

    02-test: inform the user about how many frames can we get

--- src/bowling_game/k1.clj
+++ src/bowling_game/k1.clj
@@ -1,4 +1,5 @@
 (ns bowling-game.k1)
 
 (defn create-game []
-  {:game :bowling-game})
+  {:game :bowling-game
+   :total-frames 10})
--- test/bowling_game/k1_test.clj
+++ test/bowling_game/k1_test.clj
@@ -4,3 +4,7 @@
 
 (deftest can-we-create-a-game?
   (is (= (:game (create-game)) :bowling-game)))
+
+(deftest can-we-get-info?
+  (testing "how many frames there is in a single game?"
+    (is (= 10 (:total-frames (create-game))))))
